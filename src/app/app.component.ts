2d2cd15c9a5f4155092b1eb01f2bad76f5837fe6
Eduardo <analista.sistemas@sedemi.com>
2025-01-24 17:18:52 -0500

    Estructura de carpetas e interfaces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'Report Board - Pizarra de reportes';
+  title = 'Report Board';
 }
