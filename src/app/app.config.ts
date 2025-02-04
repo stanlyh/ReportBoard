5989f960a367a81de78deafedb83ad2bcb6ad97c
stanlyh <eduardostanly@gmail.com>
2025-02-04 17:45:53 -0500

    Componentes para heavy-loaders, sidemenu, configuración de barriles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,26 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter, withViewTransitions } from '@angular/router';
+import {
+  provideHttpClient,
+  withInterceptorsFromDi,
+} from '@angular/common/http';
 
 import { routes } from './app.routes';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideRouter(
+      routes,
+      withViewTransitions({
+        skipInitialTransition: true,
+        // onViewTransitionCreated( transitionInfo ) {
+        //   console.log({transitionInfo});
+        // },
+      })
+    ),
+
+    // importProvidersFrom(HttpClientModule),
+    provideHttpClient(withInterceptorsFromDi()), provideAnimationsAsync(),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,29 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
+    path: 'dashboard',
+    loadComponent: () => import('./dashboard/dashboard.component'),
+    children: [
 
-  }
+
+
+      {
+        path: '',
+        redirectTo: 'control-flow',
+        pathMatch: 'full',
+      },
+    ],
+  },
+  {
+    path: '',
+    // redirectTo: '/dashboard',
+    redirectTo: (route) => {
+      // console.log(route);
+      // const authService = inject(AuthService);
+      // if (authService.isLoggedIn) {
+
+      return '/dashboard/material';
+    },
+    pathMatch: 'full',
+  },
 ];
