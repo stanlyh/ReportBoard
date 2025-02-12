0c823de2504e4e086e9776d227aeb667dd621447
stanlyh <eduardostanly@gmail.com>
2025-02-11 22:18:02 -0500

    Inclusión componentes para pages

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,9 +5,70 @@ export const routes: Routes = [
     path: 'dashboard',
     loadComponent: () => import('./dashboard/dashboard.component'),
     children: [
-
-
-
+      {
+        path: 'change-detection',
+        title: 'Change Detection',
+        loadComponent: () =>
+          import(
+            './dashboard/pages/change-detection/change-detection.component'
+          ),
+      },
+      {
+        path: 'control-flow',
+        title: 'Control Flow',
+        loadComponent: () =>
+          import('./dashboard/pages/control-flow/control-flow.component'),
+      },
+      {
+        path: 'defer-options',
+        title: 'Defer Options',
+        loadComponent: () =>
+          import('./dashboard/pages/defer-options/defer-options.component'),
+      },
+      {
+        path: 'defer-views',
+        title: 'Defer Views',
+        loadComponent: () =>
+          import('./dashboard/pages/defer-views/defer-views.component'),
+      },
+      {
+        path: 'user/:id',
+        title: 'User View',
+        loadComponent: () => import('./dashboard/pages/user/user.component'),
+      },
+      {
+        path: 'user-list',
+        title: 'User List',
+        loadComponent: () => import('./dashboard/pages/users/users.component'),
+      },
+      {
+        path: 'view-transition-1',
+        title: 'View Transition 1',
+        loadComponent: () =>
+          import(
+            './dashboard/pages/view-transition/view-transition1.component'
+          ),
+      },
+      {
+        path: 'view-transition-2',
+        title: 'View Transition 2',
+        loadComponent: () =>
+          import(
+            './dashboard/pages/view-transition/view-transition2.component'
+          ),
+      },
+      {
+        path: 'inputs-outputs',
+        title: 'Inputs Outputs',
+        loadComponent: () =>
+          import('./dashboard/pages/input-output/input-output.component'),
+      },
+      {
+        path: 'material',
+        title: 'Angular Material',
+        loadComponent: () =>
+          import('./dashboard/pages/material/material.component'),
+      },
       {
         path: '',
         redirectTo: 'control-flow',
---
+++ src/app/dashboard/pages/material/material.component.ts
@@ -0,0 +1,35 @@
+import { CommonModule } from '@angular/common';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatBadgeModule } from '@angular/material/badge';
+
+import {
+  MatBottomSheet,
+  MatBottomSheetModule,
+  MatBottomSheetRef,
+} from '@angular/material/bottom-sheet';
+import { OptionsBottomSheetComponent } from './ui/options-bottom-sheet/options-bottom-sheet.component';
+
+@Component({
+  selector: 'app-material',
+  standalone: true,
+  imports: [
+    CommonModule,
+    MatSlideToggleModule,
+    MatIconModule,
+    MatButtonModule,
+    MatBadgeModule,
+    MatBottomSheetModule,
+  ],
+  templateUrl: './material.component.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export default class MaterialComponent {
+  constructor(private _bottomSheet: MatBottomSheet) {}
+
+  openBottomSheet(): void {
+    this._bottomSheet.open(OptionsBottomSheetComponent);
+  }
+}
